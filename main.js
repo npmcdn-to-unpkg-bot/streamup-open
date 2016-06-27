71d34a278948e0ce5faed486cbc3b0812832f97e
CEO <beastar457@gmail.com>
2016-06-27 16:23:19 +0200

    added StreamUpBox server shipped with project

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// var shell = require('shelljs');
+// shell.exec('bash ./commands/dir.sh', {silent:false,async:true}).output;
+// if (shell.exec('bash cd ~/Desktop && sudo git init && sudo git commit -am "Auto-commit"').code !== 0) {
+// 	shell.echo('Error: Git commit failed');
+
+// };
+
 'use strict';
 var _ = require('lodash');
 var fs = require('file-system');
@@ -5,12 +12,19 @@ var app = require('app');
 var path = require('path');
 var shell = require('shelljs');
 var BrowserWindow = require('browser-window');
+var server = require('graphQl-Mysql-Server');
+var Schema = require('graphQl-Mysql-Server').Schema;
+const child_process = require('child_process');
+var graphql = require ('graphql').graphql;
 
 // ####################################################
 // ####################################################
 
 // Report crashes to our server.
+shell.exec('bash ./commands/dir.sh', {silent:false,async:true}).output;
 require('crash-reporter').start();
+
+
 
 var mainWindow = null;
 var options = {
@@ -32,18 +46,21 @@ app.on('window-all-closed', function() {
   if(process.platform !== 'darwin') { app.quit(); }
 });
 
+var query = '{addFile{addFile(firstName:"richie",lastName:"Me",email:"m@gmail.com"){id}}}';
+graphql(Schema, query).then( function(result) {
+  console.log(result);
+});
 app.on('ready', function() {
 
-  // shell.mkdir('-p','/home/$USER/Desktop','/home/echo $USER/Desktop/Me');
-
-	// var child = shell.exec('cd bash && ./dir.sh', {async:false}).output;
-	// shell.echo(child);
-
-  if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-    shell.echo('Error: Git commit failed');
-    exit(1);
-  };
-
+	//load a folder that a user defined to be a web
+  // var user = shell.echo($USER);
+  // child_process.exec('./dir.sh', ['mode=ALL'], {}, function(err, data) {
+  //   // console.log(stdout);
+	// 	shell.echo(err);
+  // });
+  // fs.mkdir('/home/richard/Desktop/Me');
+    // var output = shell.exec("bash ./bash/dir.sh",{silent:true,async:false}).output;
+    // shell.echo('Hello there');
 
   //get os home path and create folder there with the permission read,write
 
