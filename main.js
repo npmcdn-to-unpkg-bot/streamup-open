2a3898e3119649c8cb127af63540bf8d2ed0d9e1
CEO <beastar457@gmail.com>
2016-07-15 22:28:26 +0200

    Adding Views managed by Angular2

--- main.js
+++ main.js
@@ -9,9 +9,6 @@ var path = require('path');
 var shell = require('shelljs');
 var BrowserWindow = require('browser-window');
 const notifier = require('node-notifier');
-// var server = require('graphQl-Mysql-Server');
-// var Schema = require('graphQl-Mysql-Server').Schema;
-// const app=require('electron');
 const child_process = require('child_process');
 var graphql = require ('graphql').graphql;
 var chokidar= require('chokidar');
@@ -20,28 +17,19 @@ var filessystem = require('fs');
 
 
 var mainWindow = null;
-// var options = {
-// 	"debug": true   ,
-// 	"version": "1.0.0",
-// 	"views_dir": "views",
-// 	"root_view": "index.html"
-// };
-
-// options = _.extend({
-// 	// ADDITIONAL CUSTOM SETTINGS
-// }, options);
-
-// //how ti run a script in background forever
-// //understanding the app from electron!
-// app.on('window-all-closed', function() {
-//   if(process.platform !== 'darwin') { app.quit(); }
-// });
 
 
 require('crash-reporter').start();
 
 
 app.on('ready', function() {
+var options ={
+    with:800,
+    height:600,
+    debug:true
+};
+var mainWindow = new BrowserWindow(options);
+mainWindow.loadUrl('file://'+ __dirname + '/views/index.html');
 var folderWatcher = function(object) {
         fs.readdir(dir, function(err, items) {
         // console.log(items);
@@ -79,30 +67,7 @@ else
 {
     console.log("Folder already exist!");
 }
-
- 
-//        // Create the browser window and disable integration with node
-//         mainWindow = new BrowserWindow({
-//           width: 800,
-//           height: 600,
-//           nodeIntegration: false
-//         });
-//         // and load the index.html of the app.
-//         mainWindow.loadURL('file://'  , __dirname  , '/public/index.html');
- 
-//       var mkdirp = require('mkdirp');
-//       var dir=shell.exec('./dir.sh',{async:false}).output;
-//       mkdirp ('dir', function(err) {
-//       shell.echo(dir);
-        
-//     });
-//       // Emitted when the window is closed.
-//     mainWindow.on('closed', function() {
-//       // Dereference the window object, usually you would store windows
-//       // in an array if your app supports multi windows, this is the time
-//       // when you should delete the corresponding element.
-//       mainWindow = null;
-//     });
- 
+       
 });
+
 
