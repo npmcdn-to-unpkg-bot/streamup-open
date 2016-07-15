07a925219a788b84923dad08a51693c921958d5b
CEO <beastar457@gmail.com>
2016-07-15 07:57:25 +0200

    ...

--- main.js
+++ main.js
@@ -45,13 +45,29 @@ app.on('window-all-closed', function() {
 
 // app.on('ready', function() {
 
+
+//        // Create the browser window and disable integration with node
+//         mainWindow = new BrowserWindow({
+//           width: 800,
+//           height: 600,
+//           nodeIntegration: false
+//         });
+//         // and load the index.html of the app.
+//         mainWindow.loadURL('file://'  , __dirname  , '/public/index.html');
+ 
 //       var mkdirp = require('mkdirp');
 //       var dir=shell.exec('./dir.sh',{async:false}).output;
 //       mkdirp ('dir', function(err) {
 //       shell.echo(dir);
         
 //     });
-
+//       // Emitted when the window is closed.
+//     mainWindow.on('closed', function() {
+//       // Dereference the window object, usually you would store windows
+//       // in an array if your app supports multi windows, this is the time
+//       // when you should delete the corresponding element.
+//       mainWindow = null;
+//     });
 
    
 
